3ee8227db039e227d31463f019d8adc9f98d9910
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-10-10 11:28:37 +0200

    use Google Recaptcha v2

--- quintagroup/formlib/captcha/widget.py
+++ quintagroup/formlib/captcha/widget.py
@@ -1,5 +1,8 @@
-from DateTime import DateTime
+from Products.statusmessages.interfaces import IStatusMessage
 
+import urllib2
+import urllib
+import simplejson as json
 try:
     from zope.site.hooks import getSite
     getSite()
@@ -7,17 +10,15 @@ except ImportError:
     from zope.app.component.hooks import getSite
 from zope.app.form.browser import ASCIIWidget
 from zope.app.form.interfaces import ConversionError
-from zope.app.form.browser.widget import renderElement
+
 from zope.i18n import MessageFactory
 
 from Acquisition import aq_parent
 
-from Products.CMFCore.utils import getToolByName
+
 from Products.CMFCore.interfaces import ISiteRoot
 
-from quintagroup.captcha.core.utils import (decrypt, parseKey,
-                                            encrypt1, getWord,
-                                            detectInlineValidation)
+from quintagroup.captcha.core.utils import detectInlineValidation
 
 try:
     from plone.app.form import inline_validation
@@ -49,32 +50,11 @@ class CaptchaWidget(ASCIIWidget):
         return site
 
     def __call__(self):
-        kwargs = {'type': self.type,
-                  'name': self.name,
-                  'id': self.name,
-                  'cssClass': self.cssClass,
-                  'style': self.style,
-                  'size': self.displayWidth,
-                  'extra': self.extra}
-
-        site = self.get_site()
-        portal_url = getToolByName(site, 'portal_url')()
-        key = site.getCaptcha()
-
-        if self._prefix:
-            prefix = '%s.' % self._prefix
-        else:
-            prefix = ''
-
-        return u"""<input type="hidden" value="%s" name="%shashkey" />
-                   %s
-                   <img src="%s/getCaptchaImage/%s"
-                        alt="Enter the word"/>""" % (key,
-                                                     prefix,
-                                                     renderElement(self.tag,
-                                                                   **kwargs),
-                                                     portal_url,
-                                                     key)
+        sitekey = self.get_site().getProperty('RECAPTCHA_SITEKEY')
+        lang = self.request.LANGUAGE
+        form = """<script src="https://www.google.com/recaptcha/api.js?hl=%s"></script>
+        <div class="g-recaptcha" data-sitekey="%s"></div>""" % (lang, sitekey)
+        return form
 
     def hasInput(self):
         # The validator looks for the captcha only if the captcha field
@@ -98,37 +78,17 @@ class CaptchaWidget(ASCIIWidget):
 
         # Verify the user input against the captcha.
         # Get captcha type (static or dynamic)
-        site = self.get_site()
-        captcha_type = site.getCaptchaType()
+        google_dict = {'secret': self.get_site().getProperty('RECAPTCHA_SECRET')}
+        google_dict['response'] = self.request.form['g-recaptcha-response']
+        #  google_dict['remoteip'] = self.request.environ["HTTP_X_FORWARDED_FOR"]
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        data = urllib.urlencode(google_dict)
+        req = urllib2.Request(url, data)
+        recaptcha_value = urllib2.urlopen(req)
+        json_result = json.loads(recaptcha_value.read())
 
-        # validate captcha input
-        if input and captcha_type in ['static', 'dynamic']:
-            # make up form prefix
-            if self._prefix:
-                prefix = '%s.' % self._prefix
-            else:
-                prefix = ''
+        if not json_result['success']:
+            message = _('Invalid captcha value')
+            return ConversionError(message)
 
-            hashkey = self.request.get('%shashkey' % prefix, '')
-            decrypted_key = decrypt(site.captcha_key, hashkey)
-            parsed_key = parseKey(decrypted_key)
-
-            index = parsed_key['key']
-            date = parsed_key['date']
-
-            if captcha_type == 'static':
-                img = getattr(site, '%s.jpg' % index)
-                solution = img.title
-                enc = encrypt1(input)
-            else:
-                enc = input
-                solution = getWord(int(index))
-
-            captcha_tool = getToolByName(site, 'portal_captchas')
-            if (enc != solution) or (decrypted_key in captcha_tool.keys()) or \
-               (DateTime().timeTime() - float(date) > 3600):
-                raise ConversionError(_(u'Please re-enter validation code.'))
-            else:
-                captcha_tool.addExpiredKey(decrypted_key)
-
-        return super(CaptchaWidget, self)._toFieldValue(input)
+        return 'ok'
